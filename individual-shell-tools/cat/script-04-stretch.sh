877cd0c74470cc9d3ddd867fbb15822860139114
Fithi Teklom <fthiteklom@gmail.com>
2026-07-17 16:49:02 +0100

    Adjust the failed test files

--- individual-shell-tools/cat/script-04-stretch.sh
+++ individual-shell-tools/cat/script-04-stretch.sh
@@ -14,4 +14,4 @@ set -euo pipefail
 # 4 I was tempted to take a bite of it.
 # 5 But this seemed like a bad idea...
 
-cat -n individual-shell-tools/helper-files/*
+cat -n ../helper-files/*
--- individual-shell-tools/grep/script-02.sh
+++ individual-shell-tools/grep/script-02.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 # TODO: Write a command to output every line in dialogue.txt that contains the word Doctor (regardless of case).
 # The output should contain 9 lines.
 
-grep -i "^Doctor:" dialogue.txt
+grep -iw "Doctor" dialogue.txt
