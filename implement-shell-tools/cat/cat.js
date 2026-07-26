10353a9affbf5828dea418c17d7ddaa0ef8fc515
Fithi Teklom <fthiteklom@gmail.com>
2026-07-26 16:27:47 +0100

    Complete cat.js exercises.

--- implement-shell-tools/cat/cat.js
+++ implement-shell-tools/cat/cat.js
@@ -1,4 +1,35 @@
-const fs = require ("fs");
+const fs = require("fs");
+
 const args = process.argv.slice(2);
-const content = fs.readFileSync(args[0], "utf-8")
-console.log(content);
+
+let mode = "normal";
+
+if (args[0] === "-b") {
+  mode = "numberNonEmpty";
+}
+
+const files = args.slice(1);
+
+let lineNum = 1;
+
+for (const file of files) {
+
+  const content = fs.readFileSync(file, "utf8");
+
+  const lines = content.trimEnd().split("\n");
+
+  for (const line of lines) {
+
+    if (mode === "numberNonEmpty") {
+
+      if (line !== "") {
+        console.log(lineNum + " " + line);
+        lineNum++;
+      } else {
+        console.log(line);
+      }
+
+    }
+
+  }
+}
