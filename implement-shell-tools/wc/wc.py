403ac52f494bfba8402497c799edd7c2ce64fecf
Fithi Teklom <fthiteklom@gmail.com>
2026-09-04 11:06:58 +0100

    Add a function to avoid duplication.

--- implement-shell-tools/wc/wc.py
+++ implement-shell-tools/wc/wc.py
@@ -4,11 +4,36 @@ import os
 count_lines = "-l" in sys.argv
 count_words = "-w" in sys.argv
 count_bytes = "-c" in sys.argv
+
 filenames = []
 
 for argument in sys.argv[1:]:
     if not argument.startswith("-"):
         filenames.append(argument)
+
+def format_counts(line_count, word_count, byte_count):
+    output = []
+
+    if count_lines:
+        output.append(line_count)
+
+    if count_words:
+        output.append(word_count)
+
+    if count_bytes:
+        output.append(byte_count)
+
+    if not count_lines and not count_words and not count_bytes:
+        output.append(line_count)
+        output.append(word_count)
+        output.append(byte_count)
+
+    formatted_output = []
+
+    for number in output:
+        formatted_output.append(f"{number:3}")
+
+    return " ".join(formatted_output)
 
 total_lines = 0
 total_words = 0
@@ -23,55 +48,20 @@ for filename in filenames:
     for line in file:
         line_count += 1
         word_count += len(line.split())
+
     byte_count = os.path.getsize(filename)
 
     total_lines += line_count
     total_words += word_count
     total_bytes += byte_count
 
-    output = []
-
-    if count_lines:
-        output.append(line_count)
-
-    if count_words:
-        output.append(word_count)
-
-    if count_bytes:
-        output.append(byte_count)
-
-
-    if not count_lines and not count_words and not count_bytes:
-        output.append(line_count)
-        output.append(word_count)
-        output.append(byte_count)
-
-    formatted_output = []
-
-    for number in output:
-        formatted_output.append(f"{number:3}")
-
-    print(" ".join(formatted_output), filename)
+    print(
+        format_counts(line_count, word_count, byte_count),
+        filename,
+    )
 
 if len(filenames) > 1:
-    total_output = []
-
-    if count_lines:
-        total_output.append(total_lines)
-
-    if count_words:
-        total_output.append(total_words)
-
-    if count_bytes:
-        total_output.append(total_bytes)
-
-    if not count_lines and not count_words and not count_bytes:
-        total_output.append(total_lines)
-        total_output.append(total_words)
-        total_output.append(total_bytes)
-        
-    formatted_total = []
-
-    for number in total_output:
-        formatted_total.append(f"{number:3}")
-    print(" ".join(formatted_total), "total")
+    print(
+        format_counts(total_lines, total_words, total_bytes),
+        "total",
+    )
